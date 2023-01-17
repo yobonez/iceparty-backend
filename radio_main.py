56feff4ec3f3fcfd0c3d825a02d855eec4741714
yobonez <59935203+yobonez@users.noreply.github.com>
2023-01-17 14:49:20 +0100

    Removed depencendy on entries.txt file with titles
    - generally i just moved from my bash script that i used
    to run this icecast radio and was slow as hell

--- radio_main.py
+++ radio_main.py
@@ -1,11 +1,15 @@
+import songupdater2
 import mutagen
 import os
 import random
 import sys
+import subprocess
+import time
 
 radio_rootdir = "/home/bonzo/share/priv_files/radio/"
 mountpoint = ""
 mountpoint_path = ""
+icecast_mountpoint = ""
 
 lines = []
 
@@ -22,42 +26,51 @@ def get_files_and_shuffle(rootdir):
     random.shuffle(lines)
 
 def prepare_files(rootdir):
+    ret_songs = []
     global_duration = 0
     
-    ent = open(os.path.join(rootdir, "PYentries.txt"), "a+")
-    af = open(os.path.join(rootdir, "PYaudiofiles.txt"), "a+")
+    af = open(os.path.join(rootdir, "audiofiles.txt"), "a+")
 
     for filepath in lines:
         filename = os.path.basename(filepath)
         songname = os.path.splitext(filename)[0]
         duration = get_file_length(mutagen.File(filepath))
 
-        ent.write("{},{}\n".format(global_duration, songname))
+        ret_songs.append((global_duration, songname))
         af.write("file '{}'\n".format(filepath))
 
         global_duration += duration
 
-
-    ent.close()
     af.close()
 
+    return ret_songs
+
 def remove_prev_files(rootdir):
-    entries = os.path.join(rootdir, "PYentries.txt")
-    audiofiles = os.path.join(rootdir, "PYaudiofiles.txt")
-    if os.path.exists(entries):
-        os.remove(entries)
+    audiofiles = os.path.join(rootdir, "audiofiles.txt")
+    if os.path.exists(audiofiles):
         os.remove(audiofiles)
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print("No endpoint specified")
         exit(1)
+
+    
     mountpoint = sys.argv[1]
     mountpoint_path = os.path.join(radio_rootdir, mountpoint)
+    audiofiles_path = os.path.join(mountpoint_path, "audiofiles.txt")
+
+    icecast_mountpoint = "icecast://source:\$icesource\$@192.168.1.2:2139/{}".format(mountpoint)
 
     remove_prev_files(mountpoint_path)
     get_files_and_shuffle(mountpoint_path)
-    prepare_files(mountpoint_path)
+    songs = prepare_files(mountpoint_path)
+
+    #os.system("ffmpeg -re -f concat -safe 0 -i {} -c:a libmp3lame -ar 44100 -ac 2 -vn -f mp3 -map_metadata 0 -content_type 'audio/mpeg' {} &".format(audiofiles_path, icecast_mountpoint))
+
+    subprocess.Popen("ffmpeg -re -f concat -safe 0 -i {} -c:a libmp3lame -ar 44100 -ac 2 -vn -f mp3 -map_metadata 0 -content_type 'audio/mpeg' {}".format(audiofiles_path, icecast_mountpoint), shell=True, start_new_session=True, stderr=subprocess.STDOUT)
+    time.sleep(4)
+    songupdater2.title_updater_start(songs, mountpoint)
 
     
 
--- songupdater2.py
+++ songupdater2.py
@@ -2,64 +2,42 @@ import time
 import requests
 import sys
 
-if len(sys.argv) < 2:
-	print("No mountpoint specified")
-	exit(1)
-
-mountpoint = sys.argv[1]
+stream_start = float(time.time())
 
 def get_stream_time():
 	return float(time.time()) - stream_start
 
-def update_stream_title(new_title):
+def update_stream_title(new_title, mountpoint):
 	headers = {'authorization': 'Basic YWRtaW46JGljZWFkbWluJA=='}
 	url = "http://192.168.1.2:2139/admin/metadata.xsl?song={}&mount=%2F{}&mode=updinfo&charset=UTF-8".format(new_title, mountpoint)
-
 	r = requests.get(url, headers=headers)
 
 	return r
 
-songs = []
-stream_start = None
+def title_updater_start(songs, mountpoint):
+	indexx = 0
+	songs.sort(reverse=False, key=lambda x: int(x[0]))
 
-with open("/home/bonzo/share/priv_files/radio/{}/start_timestamp".format(mountpoint), "r") as f:
-	stream_start = int(f.readline())
-	print("Start timestamp: ", stream_start)
-	f.close()
+	current_song = None
 
-with open("/home/bonzo/share/priv_files/radio/{}/entries.txt".format(mountpoint), "r") as f:
-	entries = f.readlines()
-	for line in entries:
-		song = line.strip("\n").split(",")
-		songs.append((song[0], song[1]))
-	print("Songs loaded.")
-	f.close()
+	for entry in songs:
+		indexx += 1 # ahead
 
-indexx = 0
-songs.sort(reverse=False, key=lambda x: int(x[0]))
-
-current_song = None
-
-for entry in songs:
-	indexx += 1 # ahead
-
-	current_time_seconds = get_stream_time()
-
-	curr_song_start = int(songs[indexx - 1][0])
-	next_song_start = int(songs[indexx][0])
-
-	donotsend = False
-	while True:
 		current_time_seconds = get_stream_time()
-		if entry[1] == current_song:
-			donotsend = True
-		if current_time_seconds <= next_song_start:
-			if not donotsend:
-				#print("Current song: ", entry)
-				req = update_stream_title(entry[1])
-#				print(req)
-			current_song = entry[1]
-		else:
-			break
-		time.sleep(5)
+
+		curr_song_start = float(songs[indexx - 1][0])
+		next_song_start = float(songs[indexx][0])
+
+		donotsend = False
+		while True:
+			current_time_seconds = get_stream_time()
+			if entry[1] == current_song:
+				donotsend = True
+			if current_time_seconds <= next_song_start:
+				if not donotsend:
+					req = update_stream_title(entry[1], mountpoint)
+				current_song = entry[1]
+			else:
+				break
+			time.sleep(1)
 
